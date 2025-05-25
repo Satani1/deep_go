48ef93bf8e2949982afebaa6bf54ab7ffabf3462
Ali <aliev.ali21@wb.ru>
2025-05-26 00:15:29 +0300

    hw-9 serializer

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-// go test -v homework_test.go
 
 type Person struct {
 	Name    string `properties:"name"`
@@ -16,8 +17,58 @@ type Person struct {
 }
 
 func Serialize(person Person) string {
-	// need to implement
-	return ""
+	val := reflect.ValueOf(person)
+	typ := val.Type()
+	lines := make([]string, 0, typ.NumField())
+
+	for idx := 0; idx < typ.NumField(); idx++ {
+		field := typ.Field(idx)
+		key, opts := parseFieldTag(field)
+		if key == "" {
+			continue
+		}
+
+		fieldValue := val.Field(idx)
+		if shouldSkipField(opts, fieldValue) {
+			continue
+		}
+
+		valueStr := formatValue(fieldValue)
+		lines = append(lines, key+"="+valueStr)
+	}
+
+	return strings.Join(lines, "\n")
+}
+
+func parseFieldTag(field reflect.StructField) (key string, opts []string) {
+	tag := field.Tag.Get("properties")
+	if tag == "" {
+		return "", nil
+	}
+	parts := strings.Split(tag, ",")
+	return parts[0], parts[1:]
+}
+
+func shouldSkipField(opts []string, val reflect.Value) bool {
+	for _, opt := range opts {
+		if opt == "omitempty" && val.IsZero() {
+			return true
+		}
+	}
+	return false
+}
+
+func formatValue(val reflect.Value) string {
+	switch val.Kind() {
+	case reflect.String:
+		return val.String()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return fmt.Sprintf("%d", val.Int())
+	case reflect.Bool:
+		return fmt.Sprintf("%t", val.Bool())
+	default:
+		return fmt.Sprintf("%v", val.Interface())
+	}
 }
 
 func TestSerialization(t *testing.T) {
