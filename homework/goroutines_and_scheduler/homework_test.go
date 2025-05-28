c5a3f7976b3618784e72e3638cb4ef14aa7ca30c
Ali <aliev.ali21@wb.ru>
2025-05-28 22:28:09 +0300

    hw-12 scheduler

--- homework/goroutines_and_scheduler/homework_test.go
+++ homework/goroutines_and_scheduler/homework_test.go
@@ -6,31 +6,104 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// go test -v homework_test.go
 type Task struct {
 	Identifier int
 	Priority   int
 }
 
 type Scheduler struct {
-	// need to implement
+	tasks   []Task
+	taskMap map[int]int
 }
 
 func NewScheduler() Scheduler {
-	// need to implement
-	return Scheduler{}
+	return Scheduler{
+		tasks:   make([]Task, 0),
+		taskMap: make(map[int]int, 0),
+	}
 }
 
 func (s *Scheduler) AddTask(task Task) {
-	// need to implement
+	if _, exists := s.taskMap[task.Identifier]; exists {
+		return
+	}
+
+	s.tasks = append(s.tasks, task)
+	index := len(s.tasks) - 1
+	s.taskMap[task.Identifier] = index
+	s.up(index)
 }
 
 func (s *Scheduler) ChangeTaskPriority(taskID int, newPriority int) {
-	// need to implement
+	index, exists := s.taskMap[taskID]
+	if !exists {
+		return
+	}
+
+	oldPriority := s.tasks[index].Priority
+	s.tasks[index].Priority = newPriority
+
+	if newPriority > oldPriority {
+		s.up(index)
+	} else if newPriority < oldPriority {
+		s.down(index)
+	}
 }
 
 func (s *Scheduler) GetTask() Task {
-	// need to implement
-	return Task{}
+	if len(s.tasks) == 0 {
+		return Task{}
+	}
+
+	root := s.tasks[0]
+	last := len(s.tasks) - 1
+	s.tasks[0] = s.tasks[last]
+	s.taskMap[s.tasks[0].Identifier] = 0
+	s.tasks = s.tasks[:last]
+	delete(s.taskMap, root.Identifier)
+
+	if len(s.tasks) > 0 {
+		s.down(0)
+	}
+
+	return root
+}
+
+func (s *Scheduler) up(i int) {
+	for {
+		parent := (i - 1) / 2
+		if parent == i || s.tasks[parent].Priority >= s.tasks[i].Priority {
+			break
+		}
+		s.swap(i, parent)
+		i = parent
+	}
+}
+
+func (s *Scheduler) down(i int) {
+	for {
+		left := 2*i + 1
+		if left >= len(s.tasks) || left < 0 {
+			break
+		}
+		largest := left
+		if right := left + 1; right < len(s.tasks) &&
+			s.tasks[right].Priority > s.tasks[left].Priority {
+			largest = right
+		}
+		if s.tasks[i].Priority >= s.tasks[largest].Priority {
+			break
+		}
+		s.swap(i, largest)
+		i = largest
+	}
+}
+
+func (s *Scheduler) swap(i, j int) {
+	s.tasks[i], s.tasks[j] = s.tasks[j], s.tasks[i]
+	s.taskMap[s.tasks[i].Identifier] = i
+	s.taskMap[s.tasks[j].Identifier] = j
 }
 
 func TestTrace(t *testing.T) {
@@ -56,7 +129,7 @@ func TestTrace(t *testing.T) {
 	scheduler.ChangeTaskPriority(1, 100)
 
 	task = scheduler.GetTask()
-	assert.Equal(t, task1, task)
+	assert.Equal(t, Task{Identifier: 1, Priority: 100}, task)
 
 	task = scheduler.GetTask()
 	assert.Equal(t, task3, task)
