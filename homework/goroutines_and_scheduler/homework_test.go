1ef131ab2b2a337db38bfb09ac94841c3eab0b95
Ali <aliev.ali21@wb.ru>
2025-06-09 23:24:47 +0300

    [hw-12] fixes;

--- homework/goroutines_and_scheduler/homework_test.go
+++ homework/goroutines_and_scheduler/homework_test.go
@@ -12,100 +12,128 @@ type Task struct {
 	Priority   int
 }
 
-type Scheduler struct {
-	tasks   []Task
-	taskMap map[int]int
+type TaskHeap struct {
+	tasks       []Task
+	taskIndexes map[int]int
 }
 
-func NewScheduler() Scheduler {
-	return Scheduler{
-		tasks:   make([]Task, 0),
-		taskMap: make(map[int]int, 0),
+func NewTaskHeap() *TaskHeap {
+	return &TaskHeap{
+		tasks:       []Task{},
+		taskIndexes: make(map[int]int),
 	}
 }
 
-func (s *Scheduler) AddTask(task Task) {
-	if _, exists := s.taskMap[task.Identifier]; exists {
+func (t *TaskHeap) Len() int {
+	return len(t.tasks)
+}
+
+func (t *TaskHeap) up(i int) {
+	for {
+		parent := (i - 1) / 2
+		if parent == i || t.tasks[parent].Priority >= t.tasks[i].Priority {
+			break
+		}
+
+		t.swap(i, parent)
+		i = parent
+	}
+}
+
+func (t *TaskHeap) down(i int) {
+	for {
+		left := 2*i + 1
+		if left >= t.Len() || left < 0 {
+			break
+		}
+
+		largest := left
+		if right := left + 1; right < t.Len() && t.tasks[right].Priority > t.tasks[left].Priority {
+			largest = right
+		}
+
+		if t.tasks[i].Priority >= t.tasks[largest].Priority {
+			break
+		}
+
+		t.swap(i, largest)
+		i = largest
+	}
+}
+
+func (t *TaskHeap) swap(i, j int) {
+	t.tasks[i], t.tasks[j] = t.tasks[j], t.tasks[i]
+	t.taskIndexes[t.tasks[i].Identifier] = i
+	t.taskIndexes[t.tasks[j].Identifier] = j
+}
+
+func (t *TaskHeap) Push(task Task) {
+	if _, exists := t.taskIndexes[task.Identifier]; exists {
 		return
 	}
 
-	s.tasks = append(s.tasks, task)
-	index := len(s.tasks) - 1
-	s.taskMap[task.Identifier] = index
-	s.up(index)
+	t.tasks = append(t.tasks, task)
+	index := t.Len() - 1
+	t.taskIndexes[task.Identifier] = index
+	t.up(index)
 }
 
-func (s *Scheduler) ChangeTaskPriority(taskID int, newPriority int) {
-	index, exists := s.taskMap[taskID]
-	if !exists {
-		return
-	}
-
-	oldPriority := s.tasks[index].Priority
-	s.tasks[index].Priority = newPriority
-
-	if newPriority > oldPriority {
-		s.up(index)
-	} else if newPriority < oldPriority {
-		s.down(index)
-	}
-}
-
-func (s *Scheduler) GetTask() Task {
-	if len(s.tasks) == 0 {
+func (t *TaskHeap) Pop() Task {
+	if t.Len() == 0 {
 		return Task{}
 	}
 
-	root := s.tasks[0]
-	last := len(s.tasks) - 1
-	s.tasks[0] = s.tasks[last]
-	s.taskMap[s.tasks[0].Identifier] = 0
-	s.tasks = s.tasks[:last]
-	delete(s.taskMap, root.Identifier)
+	root := t.tasks[0]
+	last := t.Len() - 1
+	t.tasks[0] = t.tasks[last]
+	t.taskIndexes[t.tasks[0].Identifier] = 0
+	t.tasks = t.tasks[:last]
+	delete(t.taskIndexes, root.Identifier)
 
-	if len(s.tasks) > 0 {
-		s.down(0)
+	if t.Len() > 0 {
+		t.down(0)
 	}
 
 	return root
 }
 
-func (s *Scheduler) up(i int) {
-	for {
-		parent := (i - 1) / 2
-		if parent == i || s.tasks[parent].Priority >= s.tasks[i].Priority {
-			break
-		}
-		s.swap(i, parent)
-		i = parent
+func (t *TaskHeap) Update(identifier int, priority int) {
+	index, exists := t.taskIndexes[identifier]
+	if !exists {
+		return
+	}
+
+	oldPriority := t.tasks[index].Priority
+	t.tasks[index].Priority = priority
+
+	if priority > oldPriority {
+		t.up(index)
+	} else if priority < oldPriority {
+		t.down(index)
 	}
 }
 
-func (s *Scheduler) down(i int) {
-	for {
-		left := 2*i + 1
-		if left >= len(s.tasks) || left < 0 {
-			break
-		}
-		largest := left
-		if right := left + 1; right < len(s.tasks) &&
-			s.tasks[right].Priority > s.tasks[left].Priority {
-			largest = right
-		}
-		if s.tasks[i].Priority >= s.tasks[largest].Priority {
-			break
-		}
-		s.swap(i, largest)
-		i = largest
+type Scheduler struct {
+	taskHeap *TaskHeap
+}
+
+func NewScheduler() Scheduler {
+	return Scheduler{
+		taskHeap: NewTaskHeap(),
 	}
 }
 
-func (s *Scheduler) swap(i, j int) {
-	s.tasks[i], s.tasks[j] = s.tasks[j], s.tasks[i]
-	s.taskMap[s.tasks[i].Identifier] = i
-	s.taskMap[s.tasks[j].Identifier] = j
+func (s *Scheduler) AddTask(task Task) {
+	s.taskHeap.Push(task)
 }
 
+func (s *Scheduler) ChangeTaskPriority(taskID int, newPriority int) {
+	s.taskHeap.Update(taskID, newPriority)
+}
+
+func (s *Scheduler) GetTask() Task {
+	return s.taskHeap.Pop()
+}
 func TestTrace(t *testing.T) {
 	task1 := Task{Identifier: 1, Priority: 10}
 	task2 := Task{Identifier: 2, Priority: 20}
